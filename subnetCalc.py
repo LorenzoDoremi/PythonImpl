3f3cd70d662ecfb4dce4a1415080c9d208e712f8
Lorenzo <lorenzodoremi1994@gmail.com>
2022-06-04 15:34:14 +0200

    introducing simple subnet calculator

---
+++ subnetCalc.py
@@ -0,0 +1,26 @@
+from numpy import power
+
+# algoritmo per trovare il numero di reti e sottoreti di un IPV4 data una MASK CIDR
+def subnetCalc(a,b,c,d,mask):
+
+    bits = 32
+    if a <=126:
+      bits = 8
+    if a >= 128 and a <= 191:
+      bits = 16
+    if a >= 192 and a <= 223:
+      bits = 24
+
+    subnets = power(2, mask-bits)
+    hosts_per_subnet = power(2, 32 - mask) - 2;
+    hosts = power(2, 32 - mask)*subnets - 2* subnets
+    print(subnets)
+    print(hosts_per_subnet)
+    
+    
+
+
+subnetCalc(142,168,2,0, 18)
+
+
+      
