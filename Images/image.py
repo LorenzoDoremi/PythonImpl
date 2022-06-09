aebf0f50a5e207a1ac7256c0e3debfaab8aca2e7
Lorenzo <lorenzodoremi1994@gmail.com>
2022-06-09 14:21:41 +0200

    added creative image manipulation

---
+++ Images/image.py
@@ -0,0 +1,34 @@
+from tokenize import String
+from PIL import Image
+import tkinter
+
+from numpy import size
+
+
+
+
+im = Image.open('Images/girl.jpg')
+width, height = im.size
+
+pixel_values = list(im.getdata())
+
+
+
+# init tk
+root = tkinter.Tk()
+
+# create canvas
+myCanvas = tkinter.Canvas(root, bg="black", height=height, width=width)
+
+weights = ["", "-", "o", "x", "X"]
+i = 0
+sieve = 10
+for x in range(0,width,sieve):
+ for y in range(0,height,sieve):
+   w = weights[int((int((sum(pixel_values[y*width + x])/3))*len(weights))/255)%len(weights)]
+   myCanvas.create_text(x, y,
+                         fill="#ffffff", text=w, font=('Arial',sieve))
+
+# add to window and show
+myCanvas.pack()
+root.mainloop()
