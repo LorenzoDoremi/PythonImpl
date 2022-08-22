45363e6631b07da3b05853ad1412eba42f80dbbe
Lorenzo <lorenzodoremi1994@gmail.com>
2022-08-22 22:23:14 +0200

    added git tutorial

---
+++ Lezioni/tutorial programmazione.py
@@ -0,0 +1,6 @@
+# git init -> creo un repository
+# git add .  -> aggiungo tutti i file
+# git commit -m "nuovo commit" -> salvo i file
+# creo un repository su github e seguo istruzioni
+# git branch "nome branch" -> creo una nuova branch (in cui fare modifiche pazze :D)
+# git checkout "nome branc" -> per cambiare branch
